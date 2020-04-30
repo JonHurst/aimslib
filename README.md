1474ae120a2df709735b683f6d081adfb64056d0
Jon Hurst <jon.a@hursts.org.uk>
2020-04-30 10:20:28 +0100

    Started aimslib project

---
+++ README.md
@@ -0,0 +1,4 @@
+# aimslib #
+
+This is a refactor from various different projects to move the functionality of
+extracting information from AIMS into a common library.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aimslib",
+    version="0.0.1",
+    author="Jon Hurst",
+    author_email="jon.a@hursts.org.uk",
+    description="A library for working with AIMS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JonHurst/aimslib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
