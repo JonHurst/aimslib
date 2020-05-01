55272adf0028bd3d4e10c500028a51567d64691b
Jon Hurst <jon.a@hursts.org.uk>
2020-05-01 16:35:50 +0100

    Started adding unit tests

---
+++ tests/test_brief_roster.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+
+import datetime
+import unittest
+from aimslib.access.brief_roster import (
+    RosterEntry,
+    parse, duties,
+    BadBriefRoster,
+)
+from aimslib.common.types import Duty, TripID
+
+
+class TestBriefRosterParsing(unittest.TestCase):
+
+    def test_brief_roster_parse(self):
+        data = """\
+<html><head></head><body><div id="main_div">
+
+<div id="myday_14146"><table class="duties_table">
+<tr><td>CSBE</td><td>3:00</td></tr>
+<tr><td>&nbsp;</td><td>5:00</td></tr>
+<tr><td>B006D</td><td></td></tr>
+</table></div>
+
+<div id="myday_14147"><table class="duties_table">
+<tr><td>B086</td><td></td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+</table></div>
+
+<div id="myday_14150"><table class="duties_table">
+<tr><td>D/O</td><td></td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+</table></div>
+
+<div id="myday_14153"><table class="duties_table">
+<tr><td>LVE</td><td></td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+</table></div>
+
+<div id="myday_14157"><table class="duties_table">
+<tr><td>==&gt;</td><td></td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+<tr><td>B253</td><td></td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+</table></div>
+
+<div id="myday_14162"><table class="duties_table">
+<tr><td>ADTY</td><td>5:00</td></tr>
+<tr><td>&nbsp;</td><td>11:00</td></tr>
+</table></div>
+
+<div id="myday_14174"><table class="duties_table">
+<tr><td>FTGD</td><td></td></tr>
+</table></div>
+
+<div id="myday_13869"><table class="duties_table">
+<tr><td>FIRE</td><td>8:00</td></tr>
+<tr><td>&nbsp;</td><td>9:30</td></tr>
+<tr><td>DOOR</td><td>9:30</td></tr>
+<tr><td>&nbsp;</td><td>10:45</td></tr>
+<tr><td>G/S</td><td>10:45</td></tr>
+<tr><td>&nbsp;</td><td>11:30</td></tr>
+<tr><td>ASEC</td><td>11:30</td></tr>
+<tr><td>&nbsp;</td><td>13:00</td></tr>
+<tr><td>CRM</td><td>13:00</td></tr>
+<tr><td>&nbsp;</td><td>14:00</td></tr>
+<tr><td>SEP</td><td>14:00</td></tr>
+<tr><td>&nbsp;</td><td>16:00</td></tr>
+<tr><td>89</td><td></td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+</table></div>
+
+<div id="myday_14036"><table class="duties_table">
+<tr><td>B001T</td><td></td></tr>
+<tr><td>&nbsp;6</td><td></td></tr>
+</table></div>
+
+</div></body></html>
+"""
+        entries = parse(data)
+        self.assertEqual(entries, [
+            RosterEntry(aims_day='14146',
+                        items=['CSBE', '3:00', '5:00', 'B006D']),
+            RosterEntry(aims_day='14147', items=['B086']),
+            RosterEntry(aims_day='14150', items=['D/O']),
+            RosterEntry(aims_day='14153', items=['LVE']),
+            RosterEntry(aims_day='14157', items=['==>', 'B253']),
+            RosterEntry(aims_day='14162', items=['ADTY', '5:00', '11:00']),
+            RosterEntry(aims_day='14174', items=['FTGD']),
+            RosterEntry(aims_day='13869',
+                        items = ['FIRE', '8:00', '9:30',
+                                 'DOOR', '9:30', '10:45',
+                                 'G/S', '10:45', '11:30',
+                                 'ASEC', '11:30', '13:00',
+                                 'CRM', '13:00', '14:00',
+                                 'SEP', '14:00', '16:00',
+                                 '89']),
+            RosterEntry(aims_day='14036', items=['B001T', '6']),
+            ])
+
+
+    def test_bad_brief_roster(self):
+        data = "<html><head></head><body><p>No main div</p></body></html>"
+        with self.assertRaises(BadBriefRoster) as cm:
+            parse(data)
+        data = ("<html><head></head><body><div id='main_div'>"
+                "No class=duties_tables tables</div></body></html>")
+        with self.assertRaises(BadBriefRoster) as cm:
+            parse(data)
+        data = "Not even HTML"
+        with self.assertRaises(BadBriefRoster) as cm:
+            parse(data)
+
+
+
+class TestBriefRosterProcessing(unittest.TestCase):
+
+    def test_roster_processing(self):
+        duties_ = duties(
+            [
+                RosterEntry(aims_day='14146', items=['CSBE', '3:00', '5:00', 'B006D']),
+                RosterEntry(aims_day='14147', items=['B086']),
+                RosterEntry(aims_day='14152', items=['D/O']),
+                RosterEntry(aims_day='14153', items=['LVE']),
+                RosterEntry(aims_day='14157', items=['==>', 'B253']),
+                RosterEntry(aims_day='14158',
+                            items = ['FIRE', '8:00', '9:30',
+                                     'DOOR', '9:30', '10:45',
+                                     '89']),
+            ])
+        print(duties_)
+        self.assertEqual(duties_, [
+            Duty(start=None, finish=None,
+                 trip_id=TripID('14146', 'B006D'),
+                 sectors=None),
+            Duty(start=datetime.datetime(2018, 9, 24, 3, 0),
+                 finish=datetime.datetime(2018, 9, 24, 5, 0),
+                 trip_id=TripID('14146', 'CSBE'),
+                 sectors=None),
+            Duty(start=None, finish=None,
+                 trip_id=TripID('14147', 'B086'),
+                 sectors=None),
+            Duty(start=None, finish=None,
+                 trip_id=TripID('14157', 'B253'),
+                 sectors=None),
+            Duty(start=None, finish=None,
+                 trip_id=TripID('14158', '89'),
+                 sectors=None),
+            Duty(start=datetime.datetime(2018, 10, 6, 9, 30),
+                 finish=datetime.datetime(2018, 10, 6, 10, 45),
+                 trip_id=TripID('14158', 'DOOR'),
+                 sectors=None),
+            Duty(start=datetime.datetime(2018, 10, 6, 8, 0),
+                 finish=datetime.datetime(2018, 10, 6, 9, 30),
+                 trip_id=TripID('14158', 'FIRE'),
+                 sectors=None),
+        ])
