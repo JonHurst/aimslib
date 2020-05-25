6c2cafc8a9ad958e63dbc3db2eb0ce83f3af2a20
Jon Hurst <jon.a@hursts.org.uk>
2020-05-25 11:57:44 +0100

    Added __init__.py to aimslib/output so setuptools pick up package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aimslib",
-    version="0.0.6",
+    version="0.0.6post1",
     author="Jon Hurst",
     author_email="jon.a@hursts.org.uk",
     description="A library for working with AIMS",
